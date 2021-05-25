6766f11a352046c1482c00132fe6286b594c0e7b
ajakov <aleksandarjakovljevic@gmail.com>
2021-05-25 21:23:01 +0200

    moved html to a variable

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('<html>' +
-      '<head>' +
-      '<title>AJ</title>' +
-      '</head>' +
-      '<body>' +
-      '<p>This website is powered by Cloudflare workers!</p>' +
-      '<p>Deployed usign GitHub actions</p>' +
-      '<p><a href="https://aleksandarjakovljevic.com">Check out my personal website</a></a></p>' +
-      '</body>' +
-      '</html>', {
-    headers: { 'content-type': 'text/html' },
+  return new Response(html, {
+    headers: { 'content-type': 'text/html;charset=UTF-8' },
   })
 }
+
+const html = `<!DOCTYPE html>
+<head>
+    <title>AJ</title>
+</head>
+<body>
+  <h1>Hello World</h1>
+  <p>This website is powered by Cloudflare workers!</p>
+  <p>Deployed usign GitHub actions</p>
+  <p>
+    <a href="https://aleksandarjakovljevic.com">Check out my personal website</a></a>
+   </p>
+</body>`
