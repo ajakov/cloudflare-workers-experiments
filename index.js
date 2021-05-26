185861320090b3b0b29235e48041f8bb26d2271a
ajakov <aleksandarjakovljevic@gmail.com>
2021-05-26 23:41:37 +0200

    added url display

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response(html, {
+  let url = request.url;
+  let headers = JSON.stringify(request.headers.entries());
+  let responseHtml = html.replace('{{ url }}', url);
+  return new Response(responseHtml, {
     headers: { 'content-type': 'text/html;charset=UTF-8' },
   })
 }
@@ -19,6 +22,7 @@ const html = `<!DOCTYPE html>
   <h1>Hello World</h1>
   <p>This website is powered by Cloudflare workers!</p>
   <p>Deployed usign GitHub actions</p>
+  <p>URL: {{ url }}</p>
   <p>
     <a href="https://aleksandarjakovljevic.com">Check out my personal website</a></a>
    </p>
